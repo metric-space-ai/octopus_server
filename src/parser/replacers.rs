47d9cf3c68a21c2b98a790df32b47a86847e6c51
Michal Piotrowski <asyncfn.com@gmail.com>
2023-10-03 12:50:03 +0000

    #129 do not change funstion name in definition

--- src/parser/replacers.rs
+++ src/parser/replacers.rs
@@ -82,7 +82,7 @@ pub async fn replace_function_names(
     let mut last_saved_line = 0;
     for (i, code_line) in code_lines.iter().enumerate() {
         for function_name in &function_names {
-            if code_line.contains(function_name) {
+            if code_line.contains(function_name) && !code_line.contains("def") {
                 let new_line =
                     code_line.replace(function_name, &format!("{ai_service_id}-{function_name}"));
                 parsed_code_lines.push(new_line);
