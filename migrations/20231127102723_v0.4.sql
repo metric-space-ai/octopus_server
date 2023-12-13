a34717ae5d9a418b2cc936fa7f0d865be7e7ce10
Michal Piotrowski <asyncfn.com@gmail.com>
2023-12-13 11:40:29 +0000

    #202 merge migrations

--- migrations/20231127102723_v0.4.sql
+++ migrations/20231127102723_v0.4.sql
@@ -2,3 +2,14 @@
 
 ALTER TABLE ai_services ADD COLUMN allowed_user_ids UUID [];
 ALTER TABLE users ADD COLUMN is_invited BOOLEAN NOT NULL DEFAULT false;
+
+CREATE TABLE parameters(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(256) NOT NULL,
+    value TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp(0),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp(0)
+);
+
+CREATE INDEX parameters_name ON parameters(name);
