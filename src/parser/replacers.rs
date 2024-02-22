c36244a8309a5d01561dacb92d15cd7f565c2075
Michal Piotrowski <asyncfn.com@gmail.com>
2024-02-22 15:50:46 +0100

    #260 parser - exclude accelerate from mamba

--- src/parser/replacers.rs
+++ src/parser/replacers.rs
@@ -103,6 +103,7 @@ pub fn replace_pip(code_lines: Vec<String>) -> Vec<String> {
     for code_line in code_lines {
         if code_line.contains("pip")
             && code_line.contains("install")
+            && !code_line.contains("accelerate")
             && !code_line.contains("bpy")
             && !code_line.contains("nc_py_api")
             && !code_line.contains("reduct-py")
