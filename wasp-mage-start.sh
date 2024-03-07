6f64219737747c8fd4b53bb0d6b7d6aaa337166a
Michal Piotrowski <asyncfn.com@gmail.com>
2024-03-07 15:07:05 +0100

    #265 Make working WASP mage instance v2 p1
     #265 Make working WASP mage instance v2 p2

--- wasp-mage-start.sh
+++ wasp-mage-start.sh
@@ -4,4 +4,4 @@
 cd /wasp_mage
 DATABASE_URL=$WASP_MAGE_DATABASE_URL wasp db migrate-dev
 sed -i "s/    port: 3000,/    port: $WASP_MAGE_PORT,/g" /wasp_mage/.wasp/out/web-app/vite.config.ts
-DATABASE_URL=$WASP_MAGE_DATABASE_URL PORT=$WASP_MAGE_BACKEND_PORT wasp start
+DATABASE_URL="$WASP_MAGE_DATABASE_URL?ssl=true" PORT=$WASP_MAGE_BACKEND_PORT NODE_TLS_REJECT_UNAUTHORIZED=0 wasp start
