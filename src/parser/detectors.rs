8b4901bd8943d5c1df8fde782131120a3edfc629
Michal Piotrowski <asyncfn.com@gmail.com>
2024-06-24 10:01:43 +0200

    #377 fix threaded detection in parser p1
     #377 fix threaded detection in parser p2

--- src/parser/detectors.rs
+++ src/parser/detectors.rs
@@ -3,8 +3,7 @@ pub fn detect_app_threaded(code_lines: &Vec<String>) -> bool {
 
     for code_line in code_lines {
         if code_line.contains("app.run")
-            && code_line.contains("threaded")
-            && code_line.contains("False")
+            && (code_line.contains("threaded=False") || code_line.contains("threaded = False"))
         {
             app_threaded = false;
         }
